a0d77bfc8a546f3265823aa6281ab4d4cd73f694
negativo <davidebrr@gmail.com>
2013-12-10 21:29:36 +0100

    added bootstrap 3 support

--- jquery.mediaquerieswatcher.js/jquery.mediaquerieswatcher.js
+++ jquery.mediaquerieswatcher.js/jquery.mediaquerieswatcher.js
@@ -1,12 +1,12 @@
 (function($,window,document){
-    $.mediaquerieswatcher = function(el, data, options){
+    $.mediaquerieswatcher = function(options, el){
         var base = this;
         var rootEl = "";
         base.$el = $(el);
         base.el = el;
         base.$el.data("mediaquerieswatcher", base);
-
-        base.foundation5Rules = [
+        var lib = "";
+        base.rules= { foundation5: [
             { name: 'mobile', rule: 'and (max-width: 40em)', info: 'max-width 640px, mobile-only styles' },
             { name: 'medium', rule: 'and (min-width: 40em)', info: 'min-width 641px, medium screens' },
             { name: 'medium', rule: 'and (min-width: 40.063em) and (max-width: 64em)', info: 'min-width 641px and max-width 1024px' },
@@ -15,14 +15,31 @@
             { name: 'xlarge', rule: 'and (min-width: 90.063)', info: 'min-width 1441px, xlarge screens' },
             { name: 'xlarge', rule: 'and (min-width: 90.063) and (max-width: 120em) ', info: 'min-width 1441px, xlarge screens' },
             { name: 'xlarge', rule: 'and (min-width: 120em) ', info: 'min-width 1921px, xlarge screens' }
-        ];
-         base.checkMedia = function() {
-            for(var i in base.foundation5Rules){
-                var thisMedia = base.foundation5Rules[i];
-                if(window.matchMedia('only screen '+thisMedia.rule).matches){
-                    rootEl.find('.content').html('<div><span>screen size: </span>'+thisMedia.name+'</div><div><span>actual pixels: </span>'+thisMedia.info.replace('min-width','From').replace('and max-width','To').replace('max-width','Until')+'</div><div><span>responsive rule:</span>@media only screen '+thisMedia.rule+'</div>');
-                }
-            }
+        ],
+        bootstrap3: [
+            { name: 'xs - extra small', rule: 'and (max-width: 767px)', info: 'max-width: 767px' },
+            { name: 'sm - small', rule: 'and (min-width: 768px) and (max-width: 991px)', info: 'min-width 768px and max-width 991px' },
+            { name: 'md - medium', rule: 'and (min-width: 992px) and (max-width: 1199px)', info: 'min-width 992px and max-width 1199px' },
+            { name: 'lg - large', rule: 'and (min-width: 1200px)', info: 'min-width: 1200px' },
+        ]};
+
+        base.checkMedia = function() {
+            
+                
+                var theLib = base.rules[base.options.lib];
+                
+                
+                    
+                    for(var i in theLib){
+                        console.log(theLib[i]);
+                        var thisMedia = theLib[i];
+                        if(window.matchMedia('only screen '+thisMedia.rule).matches){
+                            rootEl.find('.content').html('<div><span>screen size: </span>'+thisMedia.name+'</div><div><span>actual pixels: </span>'+thisMedia.info.replace('min-width','From').replace('and max-width','To').replace('max-width','Until')+'</div><div><span>responsive rule:</span>@media only screen '+thisMedia.rule+'</div>');
+                        }
+                    }        
+                
+            
+            
         };
         base.setup = function(){
             $('body').append($('<div id="mediaquerieswatcher" ><div class="buttons"></div><div class="content"></div></div>'));
@@ -34,30 +51,18 @@
             base.setup();
             base.checkMedia();
         }();
-
-
         window.addEventListener('resize', this.checkMedia, false);
-
     };
     $.mediaquerieswatcher.defaultOptions = {
         lib: "foundation5",
-        position: "top-center", 
-        /* 
-            "top-left" 
-            "top-center" 
-            "top-right" 
-            "middle-left" 
-            "middle-center" 
-            "middle-right" 
-            "bottom-left" 
-            "bottom-center" 
-            "bottom-right"
-        */    
+        position: "top-center"  
     };
-    $.fn.mediaquerieswatcher = function(view, options){
+    $.fn.mediaquerieswatcher = function(options){
         return this.each(function(){
-            (new $.mediaquerieswatcher(tshis, view, options));
+            (new $.mediaquerieswatcher(this,options));
             $this = $(this);
         });
     };
 })(jQuery,window,document);
+
+
